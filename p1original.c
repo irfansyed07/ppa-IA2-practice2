528dd5ed4483b299f7d5ede33a5a0564733744db
Irfan Syed <93091192+irfansyed07@users.noreply.github.com>
2022-02-08 04:17:30 +0000

    commit

--- p1original.c
+++ p1original.c
@@ -1,2 +1,10 @@
-
+#include<stdio.h>
+int main()
+{
+    float base,height,*area;
+    printf("enter the base,height values\n");
+    scanf("%f%f",&base,&height);
+    *area = ((base+height)/2);
+    printf("the area of trinagle =%f",*area);
+    return 0;
 }
